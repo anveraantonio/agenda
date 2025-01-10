604792ac817594718d0918c60b704b0ffcb3b7a7
anveraantonio <103013837+anveraantonio@users.noreply.github.com>
2025-01-10 17:41:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Ingrese el registro de contacto");
+
+window.addEventListener("load", function() {
+    document.getElementById("registrado").addEventListener("click", function(){
+        alert("Gracias, en breve se comunicaran contigo");
+    })
+})
